7a5bc8fd32d74b3fb90452a3675b1153efda5170
Nathan <nathan60107@yahoo.com.tw>
2023-03-06 19:55:56 +0800

    update to v1.1.0
    feat: add data version check
    feat: improve whitelist, show url title, add delete btn
    feat: add reset setting double check
    fix: prevent remove popular&page if disable loadMore
    fix: fix the issue that content is block by ad

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         場外大樓過濾器
-// @description  已經厭倦看到一堆不感興趣的大樓? 這就是你在找的東西
+// @description  已經厭倦看到一堆不感興趣的大樓? 這就是你需要的
 // @namespace    nathan60107
 // @author       nathan60107(貝果)
-// @version      1.0.0
+// @version      1.1.0
 // @homepage     https://home.gamer.com.tw/creationCategory.php?owner=nathan60107&c=425332
 // @match        https://forum.gamer.com.tw/B.php?*bsn=60076*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=gamer.com.tw
@@ -11,19 +11,9 @@
 // @require      https://code.jquery.com/jquery-3.6.3.min.js
 // @grant        GM_setValue
 // @grant        GM_getValue
-// @grant        GM_xmlhttpRequest
 // @run-at       document-start
 // @noframes
 // ==/UserScript==
-
-//---------------------External libarary---------------------//
-/**
- *
- * detectIncognito v1.1.0 - (c) 2022 Joe Rutkowski <Joe@dreggle.com> (https://github.com/Joe12387/detectIncognito)
- *
- **/
-var detectIncognito = function () { return new Promise(function (t, o) { var e, n = "Unknown"; function r(e) { t({ isPrivate: e, browserName: n }) } function i(e) { return e === eval.toString().length } function a() { (void 0 !== navigator.maxTouchPoints ? function () { try { window.indexedDB.open("test", 1).onupgradeneeded = function (e) { var t = e.target.result; try { t.createObjectStore("test", { autoIncrement: !0 }).put(new Blob), r(!1) } catch (e) { /BlobURLs are not yet supported/.test(e.message) ? r(!0) : r(!1) } } } catch (e) { r(!1) } } : function () { var e = window.openDatabase, t = window.localStorage; try { e(null, null, null, null) } catch (e) { return r(!0), 0 } try { t.setItem("test", "1"), t.removeItem("test") } catch (e) { return r(!0), 0 } r(!1) })() } function c() { navigator.webkitTemporaryStorage.queryUsageAndQuota(function (e, t) { r(t < (void 0 !== (t = window).performance && void 0 !== t.performance.memory && void 0 !== t.performance.memory.jsHeapSizeLimit ? performance.memory.jsHeapSizeLimit : 1073741824)) }, function (e) { o(new Error("detectIncognito somehow failed to query storage quota: " + e.message)) }) } function d() { void 0 !== Promise && void 0 !== Promise.allSettled ? c() : (0, window.webkitRequestFileSystem)(0, 1, function () { r(!1) }, function () { r(!0) }) } void 0 !== (e = navigator.vendor) && 0 === e.indexOf("Apple") && i(37) ? (n = "Safari", a()) : void 0 !== (e = navigator.vendor) && 0 === e.indexOf("Google") && i(33) ? (e = navigator.userAgent, n = e.match(/Chrome/) ? void 0 !== navigator.brave ? "Brave" : e.match(/Edg/) ? "Edge" : e.match(/OPR/) ? "Opera" : "Chrome" : "Chromium", d()) : void 0 !== document.documentElement && void 0 !== document.documentElement.style.MozAppearance && i(37) ? (n = "Firefox", r(void 0 === navigator.serviceWorker)) : void 0 !== navigator.msSaveBlob && i(39) ? (n = "Internet Explorer", r(void 0 === window.indexedDB)) : o(new Error("detectIncognito cannot determine the browser")) }) };
-//---------------------External libarary---------------------//
 
 let $ = jQuery
 let dd = (...d) => {
@@ -32,17 +22,25 @@ let dd = (...d) => {
 }
 
 /**
- * @param {boolean} bool 
+ * @param {boolean} bool
  */
 function isCheck(bool) {
   return bool ? 'checked' : ''
 }
 /**
- * @param {string} condi 
- * @param {string} curr 
+ * @param {string} condi
+ * @param {string} curr
  */
 function isSelected(condi, curr) {
   return condi === curr ? 'selected' : ''
+}
+/**
+ * @param {string|undefined} a 
+ * @param {string} b 
+ */
+function versionCompare(a, b) {
+  if (!a) return -1
+  return a.localeCompare(b, undefined, { numeric: true, sensitivity: 'base' })
 }
 /**
  * @param {number} reply
@@ -65,12 +63,23 @@ function userCondition(reply, like) {
     )
   }
 }
+/** @param {{title: string, snA: string}[]} whitelist */
+function whitelistToString(whitelist) {
+  return whitelist.map(wl =>
+    `<li>
+      - 
+      <a href="https://forum.gamer.com.tw/C.php?bsn=60076&snA=${wl.snA}">${wl.title}</a>
+      <button data-snA="${wl.snA}">刪除</button>
+    </li>`
+  ).join('')
+}
 
 /**
  * @type {defaultSetting}
  */
 let setting = {}
 const defaultSetting = {
+  dataVersion: GM_info.script.version,
   enableLoadMore: true,
   enableFilter: true,
   filterReply: true,
@@ -78,11 +87,13 @@ const defaultSetting = {
   filterLike: false,
   likeCondition: 'and',
   likeLimit: 1000,
+  /** @type {{title: string, snA: string}[]} */
   whitelist: []
 }
 const settingKey = Object.keys(defaultSetting)
 let postSet = new Set()
 let removeCount = 0
+let resetCoundown = -1
 let url = new URLSearchParams(window.location.href)
 let pageIndex = +(url.get('page') ?? 1)
 
@@ -94,25 +105,36 @@ function initSettingPanel() {
       GM_setValue(key, defaultSetting[key])
     }
   }
+  if (versionCompare(setting.dataVersion, '1.1.0') < 0) {
+    resetSetting(false)
+    toastr.info('由於版本更新，設定已重置，若持續出現此通知請回報錯誤。')
+  }
 
-  $('.b-popular, .b-pager').hide()
-  $('#BH-slave').append(`
+  if (setting.enableLoadMore) $('.b-popular, .b-pager').hide()
+  $('.BH-qabox1').after(`
   <style>
+    #no-building, #no-building .setting-building, #no-building ul {
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
+    }
+    #no-building {
+      padding: 18px 12px;
+    }
+    #no-building .setting-building {
+      padding-left: 10px;
+    }
     #no-building label {
       display: block;
     }
-    #no-building :not(input) {
-      margin-top: 10px;
-      margin-bottom: 10px;
-    }
-    #no-building .setting-building > * {
-      margin-left: 15px;
-    }
-    #no-building .setting-building > select {
-      margin: 0 0 0 60px;
-    }
     #no-building input[type="number"] {
       width: 4em;
+    }
+    #no-building button, #no-building select {
+      width: max-content;
+    }
+    #no-building .setting-building select {
+      margin-left: 45px;
     }
     #no-building input::-webkit-outer-spin-button,
     #no-building input::-webkit-inner-spin-button {
@@ -142,29 +164,34 @@ function updateSettingPanel() {
     <input ${isCheck(setting.enableFilter)} data-key="enableFilter"  type="checkbox" >
     啟用大樓過濾
   </label>
-  <div class="setting-building">大樓過濾條件：
+  大樓過濾條件：
+  <div class="setting-building">
     <label>
       <input ${isCheck(setting.filterReply)} data-key="filterReply"  type="checkbox">
       回覆超過<input value="${setting.replyLimit}" data-key="replyLimit"  type="number">樓
     </label>
+    <select data-key="likeCondition">
+      <option value="and" ${isSelected(setting.likeCondition, 'and')}>且</option>
+      <option value="or" ${isSelected(setting.likeCondition, 'or')}>或</option>
+    </select>
     <label>
       <input ${isCheck(setting.filterLike)} data-key="filterLike"  type="checkbox">
-      <select data-key="likeCondition">
-        <option value="and" ${isSelected(setting.likeCondition, 'and')}>且</option>
-        <option value="or" ${isSelected(setting.likeCondition, 'or')}>或</option>
-      </select>
       推文超過<input value="${setting.likeLimit}" data-key="likeLimit"  type="number">次
     </label>
-    <div>白名單文章snA：[${setting.whitelist}]</div>
+    白名單文章：
+    <ul>
+      ${whitelistToString(setting.whitelist)}
+    </ul>
     <input class="setting-whitelist-input" placeholder="在此處貼上文章網址" type="text">
     <button class="setting-add-whitelist" type="button">新增白名單</button>
-    <button class="setting-rm-whitelist" type="button">刪除白名單</button>
   </div>
-  <button class="setting-reset" type="button">重置設定</button>
+  <button class="setting-reset" type="button">${resetCoundown === -1 ? '重置設定' : `確定? ${resetCoundown}`}</button>
   `)
-  $('#no-building .setting-reset').on('click', resetSetting)
+  $('#no-building .setting-reset').on('click', confirmReset)
   $('#no-building .setting-add-whitelist').on('click', () => modifyWhitelist('add'))
-  $('#no-building .setting-rm-whitelist').on('click', () => modifyWhitelist('delete'))
+  for (const { snA } of setting.whitelist) {
+    $(`[data-snA="${snA}"]`).on('click', () => modifyWhitelist(snA))
+  }
   for (const [key, val] of Object.entries(defaultSetting)) {
     if (Array.isArray(val)) continue
 
@@ -183,45 +210,75 @@ function saveSetting() {
   }
   toastr.info('大樓過濾設定已更新')
 }
-function resetSetting() {
+function confirmReset() {
+  // First click of reset setting, countdown
+  if (resetCoundown === -1) {
+    resetCoundown = 10
+    updateSettingPanel()
+    let itv = setInterval(() => {
+      resetCoundown--
+      updateSettingPanel()
+      if (resetCoundown === -1) clearInterval(itv)
+    }, 1000)
+    // Second click, reset setting and reload
+  } else {
+    resetSetting()
+  }
+}
+function resetSetting(reload = true) {
   for (const key of settingKey) {
     GM_setValue(key, defaultSetting[key])
   }
-  location.reload()
+  if (reload) location.reload()
 }
 /**
- * @param {'add'|'delete'} action 
+ * If action is add, add url in input to whitelist.
+ * Or, delete whiltelist item that snA == action
+ * @param {'add'|string} action
  */
 function modifyWhitelist(action) {
-  let tempList = new Set(setting.whitelist)
-  let input = $('.setting-whitelist-input').val().match(/snA=(\d+)/)
-  if (!input) {
-    return
-  }
   if (action === 'add') {
-    tempList.add(input[1])
-  } else if (action === 'delete') {
-    tempList.delete(input[1])
+    let tempList = _.cloneDeep(setting.whitelist)
+    let input = $('.setting-whitelist-input').val().match(/snA=(\d+)/)
+    if (!input) return
+
+    let snA = input[1]
+    $.ajax({
+      url: `https://forum.gamer.com.tw/C.php?bsn=60076&snA=${snA}`
+    }).done((resHTML) => {
+      let title = $(resHTML).filter('title').text()
+        .replace(' @場外休憩區 哈啦板 - 巴哈姆特', '')
+        .replace(/^【.*】/, '')
+      tempList.push({ title, snA })
+      if (_.isEqual(setting.whitelist, tempList)) {
+        toastr.warning('白名單已存在')
+      } else {
+        setting.whitelist = _.uniqBy(tempList, 'snA')
+        updateSettingPanel()
+        saveSetting()
+      }
+    })
+  } else {
+    _.pullAllBy(setting.whitelist, [{ snA: action }], 'snA')
+    updateSettingPanel()
+    saveSetting()
   }
-  setting.whitelist = Array.from(tempList)
-  updateSettingPanel()
-  saveSetting()
 }
 
 /**
  * @param {HTMLElement} html
- * @param {'append'|'remove'} mode 
+ * @param {'append'|'remove'} mode
  */
 function processHtml(html, mode) {
-  let id = $(html).find('.b-list__time__edittime > a').attr('href').match(/snA=([\d]+)/)[1]
+  let snA = $(html).find('.b-list__time__edittime > a').attr('href').match(/snA=([\d]+)/)[1]
   let title = $(html).find('.b-list__main__title').text()
   let top = $(html).hasClass('b-list__row--sticky')
   let like = $(html).find('.b-list__summary__gp').text()
   let replyRaw = $(html).find('.b-list__time__edittime > a').attr('href').match(/tnum=([0-9]+)&/)
   let reply = replyRaw ? parseInt(replyRaw[1]) - 1 : 0
-  let inWhitelist = _.includes(setting.whitelist, id) === true
+  let inWhitelist = _.filter(setting.whitelist, ['snA', snA]).length !== 0
   // Duplicated, skip it
-  if (postSet.has(id)) return
+  if (postSet.has(snA)) return
   // Should be filtered
   if (setting.enableFilter && !top && !inWhitelist &&
     userCondition(reply, like)) {
@@ -231,7 +288,7 @@ function processHtml(html, mode) {
   } else {
     if (mode === 'append') $('.b-list > tbody').append(html)
   }
-  postSet.add(id)
+  postSet.add(snA)
 }
 
 function htmlToPost() {
@@ -249,18 +306,16 @@ function loadMore(entries) {
 
   pageIndex++
   url.set('page', pageIndex)
-  GM_xmlhttpRequest({
-    method: 'GET',
-    url: decodeURIComponent(url.toString()),
-    onload: (result) => {
-      $(result.response).find('.b-list__row:not(:has(.b-list_ad))').map(
-        function f() { processHtml(this, 'append') }
-      )
-      updateSettingPanel()
-      // Register lazyload img and draw non-image thumbnail
-      Forum.B.lazyThumbnail()
-      Forum.Common.drawNoImageCanvas()
-    },
+  $.ajax({
+    url: decodeURIComponent(url.toString())
+  }).done((resHTML) => {
+    $(resHTML).find('.b-list__row:not(:has(.b-list_ad))').map(
+      function f() { processHtml(this, 'append') }
+    )
+    updateSettingPanel()
+    // Register lazyload img and draw non-image thumbnail
+    Forum.B.lazyThumbnail()
+    Forum.Common.drawNoImageCanvas()
   })
 }
 
